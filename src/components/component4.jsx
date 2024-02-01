d97d39018170310ca72fdaeb215b668f4a28d9e1
Shiwans Trivedi <120354831+artifcialmind@users.noreply.github.com>
2024-02-02 00:43:10 +0530

    Update component4.jsx

--- src/components/component4.jsx
+++ src/components/component4.jsx
@@ -7,7 +7,7 @@ const LightWeight = (props) => {
         <>
             <div id='content'>
                 <div id='content-div'>
-
+                    <h3>Open In PC for the best experience..</h3>
                     <p id='content-text'>Entering a Solar System Hold Tight..</p>
                </div>
             </div>
