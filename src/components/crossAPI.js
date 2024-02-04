756af1a7f5462fd9da87cb4028625348d3b5e00c
Shiwans Trivedi <120354831+artifcialmind@users.noreply.github.com>
2024-02-04 12:40:47 +0530

    Update crossAPI.js

--- src/components/crossAPI.js
+++ src/components/crossAPI.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 
 
 const api = axios.create({
-    baseURL: 'https://shiwans-portfolio.onrender.com',
+    // baseURL: 'https://shiwans-portfolio.onrender.com',
+    baseURL: 'https://shiwansportfolio-mrveli24.b4a.run/',
 })
 
 export default api;
